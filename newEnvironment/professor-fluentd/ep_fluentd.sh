a1479657c935278888e5ef29a12bb4a8418a7453
Girasolo <s280117@studenti.polito.it>
2024-03-20 19:08:32 +0100

    entrypoints fixed

--- newEnvironment/professor-fluentd/ep_fluentd.sh
+++ newEnvironment/professor-fluentd/ep_fluentd.sh
@@ -2,6 +2,9 @@
 
 fluentd -c fluentd/etc/fluent.conf &
 
-ip route add default via 10.200.0.10
+ip route change default via 10.200.0.10
+
+
+
 
 sleep infinity
--- newEnvironment/servidor/entrypoints/ep_servidor.sh
+++ newEnvironment/servidor/entrypoints/ep_servidor.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+
 apt-get install -y linux-headers-$(uname -r)
 
 mount -t debugfs none /sys/kernel/debug
+
+ip route change default via 10.100.0.10
+
+
+#hping3 -c 3 -S --flood 127.0.0.1 &
+#hping3 -c 3 -2 -p 80 --flood 127.0.0.1 &
+#hping3 -c 3 -1 -a 8.8.8.8 --flood 127.0.0.1 &
+#hping3 -c 3 -A --flood 127.0.0.1 &
+
 
 /bin/sleep infinity
